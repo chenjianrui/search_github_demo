2339655dc6efbe8a56759e6a132faf4073f0f738
chenjianrui <sky75217@gmail.com>
2020-09-09 23:23:08 +0800

    建置 Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import loginImg from '../images/login-img.svg'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <Wrapper>
+      <div className="container">
+        <img src={loginImg} alt='github user'/>
+        <h1>github user</h1>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`
 
 export default Login
