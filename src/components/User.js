c5567708b82fb9f910d6ed0a550886e37a7e8dde
chenjianrui <sky75217@gmail.com>
2020-09-10 15:01:52 +0800

    新增 Card component 來放置 User

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Card from './Card'
 
 const User = () => {
   return (
-    <div>
-      User page
-    </div>
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Card />
+      </Wrapper>
+    </section>
   )
 }
+
+const Wrapper = styled.section`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
 
 export default User
